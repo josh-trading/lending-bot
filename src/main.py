0ac06a8dde10779a8f5b3d7a8190f2d9cc657b56
ryanycw <ryan@ryanycw.dev>
2023-12-12 01:18:23 +0800

    build: add new constants for offer

--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
+import os
+import sys
+from dotenv import load_dotenv
+
 import threading
 import asyncio
+from constants import LENDING_CRITERIA, LENDING_PERIOD
 from library.market.candle import CandleSubscriber
+from library.account.wallet import WalletSubscriber
+
+# Add the path to the project directory to the Python path
+project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_dir)
+load_dotenv()
 
 
 async def start():
     test_key = "trade:1m:fUSD:a30:p2:p30"
     # Create an instance of the CandleSubscriber class
-    candle_subscriber = CandleSubscriber(test_key)
-    await candle_subscriber.start()
+    # candle_subscriber = CandleSubscriber(test_key)
+    # await candle_subscriber.start()
+    # Create an instance of the WalletSubscriber class
+    wallet_subscriber = WalletSubscriber(
+        config={"lending_criteria": LENDING_CRITERIA, "lending_period": LENDING_PERIOD}
+    )
+    await wallet_subscriber.start()
 
 
 def start_async_loop():
